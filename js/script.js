1b45b0adb9b1f72520f2a72f3452f7cbf27d206d
Kirill Zemko <therealkirill@gmail.com>
2022-11-16 15:01:36 +0200

    add editorconfig

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '1');
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
 
 const lastViewedmovie = prompt('Один из последних просмотренных фильмов?', 'Logan'),
-    rateLastViewedMovie = +prompt('На сколько оцените его?', '8.2'),
-    lastViewedmovie2 = prompt('Один из последних просмотренных фильмов?', 'Logan'),
-    rateLastViewedMovie2 = +prompt('На сколько оцените его?', '8.2');
+  rateLastViewedMovie = +prompt('На сколько оцените его?', '8.2'),
+  lastViewedmovie2 = prompt('Один из последних просмотренных фильмов?', 'Logan'),
+  rateLastViewedMovie2 = +prompt('На сколько оцените его?', '8.2');
 
 personalMovieDB.movies[lastViewedmovie] = rateLastViewedMovie;
-personalMovieDB.movies[lastViewedmovie2] = rateLastViewedMovie2;    
+personalMovieDB.movies[lastViewedmovie2] = rateLastViewedMovie2;
 
 console.log(personalMovieDB.movies);
+
+let num = 50;
+
+while (num < 55) {
+  console.log(num);
+  num++;
+}
