a28497ab375840bcbd0a514000a4f9dd91e90cbc
Kirill Zemko <therealkirill@gmail.com>
2022-11-18 20:48:35 +0200

    add automatisation qustetions of viewed films

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '1');
+/* Задание на урок:
 
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -8,19 +21,26 @@ const personalMovieDB = {
   privat: false
 };
 
-const lastViewedmovie = prompt('Один из последних просмотренных фильмов?', 'Logan'),
-  rateLastViewedMovie = +prompt('На сколько оцените его?', '8.2'),
-  lastViewedmovie2 = prompt('Один из последних просмотренных фильмов?', 'Logan'),
-  rateLastViewedMovie2 = +prompt('На сколько оцените его?', '8.2');
+//  && lastViewedMovie != null && rateLastViewedMovie != null && lastViewedMovie.length > 50
 
-personalMovieDB.movies[lastViewedmovie] = rateLastViewedMovie;
-personalMovieDB.movies[lastViewedmovie2] = rateLastViewedMovie2;
+
+for (let i = 1; i <= numberOfFilms; i++) {
+  const lastViewedMovie = prompt('Один из последних просмотренных фильмов?', ''),
+        rateLastViewedMovie = +prompt('На сколько оцените его?', '');//  && lastViewedMovie != null && rateLastViewedMovie != null && lastViewedMovie.length > 50
+
+ if (lastViewedMovie != '' && rateLastViewedMovie != '' || (lastViewedMovie != null && rateLastViewedMovie != null && lastViewedMovie.length > 50)) {
+  personalMovieDB.movies[lastViewedMovie] = rateLastViewedMovie;
+ }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB.movies);
-
-let num = 50;
-
-while (num < 55) {
-  console.log(num);
-  num++;
-}
