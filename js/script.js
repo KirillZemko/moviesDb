b6c831161e398b1a54c2f8d2d6d17e3593b6a64f
Kirill Zemko <therealkirill@gmail.com>
2022-11-19 11:03:33 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -21,15 +21,14 @@ const personalMovieDB = {
   privat: false
 };
 
-//  && lastViewedMovie != null && rateLastViewedMovie != null && lastViewedMovie.length > 50
-
-
 for (let i = 1; i <= numberOfFilms; i++) {
   const lastViewedMovie = prompt('Один из последних просмотренных фильмов?', ''),
-        rateLastViewedMovie = +prompt('На сколько оцените его?', '');//  && lastViewedMovie != null && rateLastViewedMovie != null && lastViewedMovie.length > 50
+        rateLastViewedMovie = +prompt('На сколько оцените его?', '');
 
  if (lastViewedMovie != '' && rateLastViewedMovie != '' || (lastViewedMovie != null && rateLastViewedMovie != null && lastViewedMovie.length > 50)) {
   personalMovieDB.movies[lastViewedMovie] = rateLastViewedMovie;
+ } else {
+  i--;
  }
 }
 
